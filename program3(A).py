1c11e857c54bd8dc0f75851c569505080dec0532
Somashekarbm <121337385+Somashekarbm@users.noreply.github.com>
2023-07-28 14:58:23 +0530

    Create program3(A).py

---
+++ program3(A).py
@@ -0,0 +1,13 @@
+#program 3(A)
+sentence=input("enter a sentence:")
+wordList=sentence.split(" ")
+print("this sentence has",len(wordList),"words")
+digcnt=upcnt=locnt=0
+for ch in sentence:
+    if '0'<=ch<='9':
+        digcnt+=1
+    elif 'A'<=ch<='Z':
+        upcnt+=1
+    elif 'a'<=ch<='z':
+        locnt+=1
+print("this sentence has:",digcnt,"digits",upcnt,"upper case letters",locnt,"lower case letters")
